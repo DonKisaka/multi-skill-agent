e73cd638cec907010d0521f20b53a33a4c307d66
DonaldKisaka <davidkisaka4@gmail.com>
2026-05-18 12:14:35 +0300

    Initial commit

---
+++ multi-skill-agent/src/main/java/com/donald/multi_skill_agent/MultiSkillAgentApplication.java
@@ -0,0 +1,13 @@
+package com.donald.multi_skill_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiSkillAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiSkillAgentApplication.class, args);
+	}
+
+}
---
+++ multi-skill-agent/src/test/java/com/donald/multi_skill_agent/MultiSkillAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donald.multi_skill_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiSkillAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
